d212708fa404821d8de29757b76182b9ce5271f8
Bing Yang <bingyang@nanometrics.ca>
2024-06-14 23:53:01 -0400

    Initial Commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About this app</h1>
+
+	<p>
+		This is an app used to keep track of your everyday trading records. You can add, edit, and delete your trades.
+	</p>
+
+</div>
---
+++ src/routes/summary/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>Trade summary Today</title>
+	<meta name="description" content="Trade summary today" />
+</svelte:head>
+
+<div>
+    <h1>Trade summary Today</h1>
+    <p>
+        This is a summary of your trades today.
+</div>
