0b6bd3ad56765de29fba561d7f5268a6f12926e8
Bing Yang <bingyang@nanometrics.ca>
2024-06-20 08:59:29 -0400

    Add P/L and update style

--- src/routes/trade-list/TradeItem.svelte
+++ src/routes/trade-list/TradeItem.svelte
@@ -2,21 +2,37 @@
     import { TradeStatus, type OptionTrade } from "../trade-detail/trade";
 
     export let trade: OptionTrade;
+    function calculatePL() {
+        return (trade.marketValue - trade.premium)*100;
+    }
+    function calculatePLPercentage() {
+        return (trade.marketValue - trade.premium)*100 / trade.premium;
+    }
 
 </script>
 
 <!-- display the summary of the trade -->
-<div class=container style="background-color: {trade.status === TradeStatus.OPEN ? 'lightgreen':'lightgrey'};"> 
+<div class="container highlight-{calculatePL() >0 ? 'green':'blue'}" style="border-style: dotted; border-color: {trade.status === TradeStatus.OPEN ? 'orange':'blue'};"> 
     <!--e.g. SPY $100 CALL 20240101 -->
      <label for="symbol" style="font-weight: bold;">{trade.symbol} ${trade.strike} {trade.optionType} {trade.expirationDate}</label>
-     <div>
+     <div style="font-size: small;">
         <label for="quantity">Quantity:</label> <span>{trade.quantity}</span>
         <label for="marketValue">Market Value:</label> <span>${trade.marketValue}</span>
         <label for="premium">Premium:</label> <span>${trade.premium}</span>
      </div>
+     <div style="font-size: small;">
+        <lable for="P/L">P/L: ${calculatePL().toFixed(2)}</lable>
+        <lable for="P/L Ratio">P/L: {calculatePLPercentage().toFixed(2)}%</lable>
+     </div>
 </div>
 
 <style>
+    .highlight-green {
+        background-color: lightgreen;
+    }
+    .highlight-red {
+        background-color: lightcoral;
+    }
     .container {
         
         margin: 0 auto;
